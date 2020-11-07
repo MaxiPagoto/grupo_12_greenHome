984c110ca41a403150fcda8133a9756ea6ff06ee
Maximiliano Pagoto <maxpagoto@gmail.com>
2020-11-07 18:08:20 -0300

    agregamos ejs carga producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,9 @@
 const productController = {
     article: function (req,res,next){
         res.render('products/product')
+      },
+      create: function(req,res, next){
+        res.render('products/create')
       }
 }
 module.exports = productController;
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,11 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const productController = require ('../controllers/productController')
 
 router.get('/', productController.article);
 
+router.get('/create', productController.create);
+
 module.exports = router;
+
