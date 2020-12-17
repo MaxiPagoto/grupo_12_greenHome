639813b440953f7af3022cee5824fb6589cc276a
alepizarro32 <alepizarro32@gmail.com>
2020-12-16 23:58:45 -0300

    Validaciones de login y register terminadas. Cookies y Session servido para trabajar.

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session')
 const methodOverride = require('method-override'); // Para usar Metodos PUT y DELETE
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productRouter = require('./routes/product');
+const cookiesCheck = require('./middlewares/cookiesCheck')
 
 const app = express();
 
@@ -22,7 +24,13 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'Mmm',
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(cookieParser());
+app.use(cookiesCheck);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,67 @@
+const {check, validationResult, body} = require('express-validator');
+const bycrypt = require('bcrypt');
+
+const fs = require('fs');
+const path = require('path')
+const usersFilePath = path.join(__dirname,'../data/users_GreenHome.json');
+const usersList = JSON.parse(fs.readFileSync(usersFilePath, {encoding:'utf-8'}));
+
+const writeData = function(data,filePath){
+    let stringUsers = JSON.stringify(data, null," ");
+    fs.writeFileSync(filePath, stringUsers)
+};
+const newID = function(){
+    let lastID = (usersList[usersList.length-1].id)
+    return ++lastID
+;}
+
+
 const userController = {
     register: function (req,res,next){
         res.render('users/register')
     },
+    processRegister: function(req,res,next){
+        let errors = validationResult(req);
+          
+        if (!errors.isEmpty()){
+            console.log(errors.errors)
+            return res.render('users/register', {errors:errors.errors})
+        } else {
+            newUserID = newID();
+            let newUser ={
+                id: newUserID,
+                email:req.body.email,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                category: "guest",
+                password:bycrypt.hashSync(req.body.password, 10),
+                avatar:'default-image.png'
+            }
+            if (req.files[0]){
+                newUser.avatar=req.files[0].filename; 
+            }
+            let usersToSave = [...usersList, newUser];
+            writeData(usersToSave, usersFilePath);
+            //return res.render('user/profile', {user:newUser})};
+            return res.send(usersToSave)
+        }
+    },
+
     login: function (req,res,next){
         res.render('users/login')
+    },
+    processLogin: (req, res) => {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.render('users/login', {errors:errors.errors})
+        }else{
+            let userFound = usersList.find(function(user){return user.email == req.body.email})
+            req.session.userLogged = userFound.email;
+            if (req.body.remember!=undefined){
+            res.cookie('userEmail',userFound.email,{maxAge:1000*60});
+            };
+            return res.send("Listo")//res.render('user/profile', {user:userFound})
+        }
     }
 
 }
