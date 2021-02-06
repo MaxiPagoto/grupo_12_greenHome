e5676ad7ca2e2d8398d21ed59aa9a8952003f31f
alepizarro32 <alepizarro32@gmail.com>
2021-02-06 16:03:13 -0300

    Validación de LOGIN testeada y funcionando.

--- public/js/js-login.js
+++ public/js/js-login.js
@@ -8,39 +8,108 @@ let emailError = document.querySelector('#email-error')
 let password = document.querySelector('#password')
 let passwordError = document.querySelector('#password-error')
 
-formulario.addEventListener('submit', function(e){
-    const error = []
+const colorVerde  = '#EAFAF1'
+const colorRojo = '#F9EBEA'
 
-    if(password.value == ''){
-        passwordError.style.opacity = '100'
-        error.push(1)
+
+//FUNCIONES
+
+const validationKey = function(variable,condition){
+    if(condition){
+        variable.style.backgroundColor = colorVerde;
+        variable.style.transition = "0.5s 0s ease";
+    }else{
+        variable.style.backgroundColor = colorRojo
+        variable.style.transition = "0.5s 0s ease";
     }
-    
-    
+}
 
-    fetch(URL+'/api/users')
-        .then(function(response){
-            return response.json();
-        }).then(function(users){
-            const userslogged = users.data
-            const usuario = []
-            for(user of userslogged){
-                if(user.email == email.value){
-                    usuario.push(user.email)
-                
-                }
+const msjCampo = function(imput,msj){
+    msj.onmouseover= function(){      
+        imput.style.opacity = '100';
+        imput.style.transition = "opacity 0.15s 0s ease";    
+    }
+    msj.onmouseout= function(){
+        imput.style.opacity = '0'
+    } 
+ }
 
-                if(usuario.length == 0 || email.value == ''){
-                    emailError.style.opacity='100'
-                    error.push(1)
-                }
+const validationBlur = function(variable,condition){
+        if(condition){
+            variable.style.backgroundColor = colorVerde;
+            variable.style.transition = "0.5s 0s ease";
+        }else{
+            variable.style.backgroundColor = colorRojo;
+            variable.style.transition = "0.5s 0s ease"
+        }
+    }
 
-                console.log('error',error)
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+let getExtension = function(value){
+    return value.slice((value.lastIndexOf(".") - 1 >>> 0) + 2)}
+
+
+// EVENTOS
+
+msjCampo(emailError,email)
+msjCampo(passwordError,password)
+
+email.onkeydown = function(){
+    validationKey(email,(email.value.match(pattern)))
+};
+
+email.onblur = function(){
+    validationBlur(email,email.value.length>0 &&(email.value.match(pattern)) )
+}
+
+password.onkeyup = function(){
+    validationKey(password,password.value.length>5)
+}
+
+
+let usersList = []
+fetch(URL+'/api/users')
+    .then(function(response){
+        return response.json();
+    }).then(function(users){
+        usersList = users.data
+    })
+
+// EVENTOS SUBMIT
+formulario.addEventListener('submit', function(e){
+    const errors = []
+    if(password.value.length<6){
+        passwordError.innerHTML  += " de al menos 6 caracteres"
+        passwordError.style.opacity = '100'
+        passwordError.style.color = "rgb(192, 57, 43)"
+        errors.push(1)
+    }
+            let userFind = usersList.find((user)=>{
+                return user.email == email.value
+            })
             
+            if(!userFind){
+                errors.push(2)
             }
-           
-        })
-        if(error.length > 0){
+
+            if(!userFind){
+                    emailError.style.opacity='100'
+                    emailError.style.color="rgb(192, 57, 43)"
+                    emailError.innerHTML = "Usuario no registrado"
+                    email.style.backgroundColor = colorRojo;
+                    email.style.transition = "0.5s 0s ease"
+                    password.style.backgroundColor = colorRojo;
+                    password.style.transition = "0.5s 0s ease"
+                } 
+            
+            if (!(email.value.match(pattern))){
+                emailError.innerHTML = "Ingrese un email válido"
+                errors.push(3)
+            }
+            
+
+        if(errors.length > 0){
             e.preventDefault();     
         }
        
