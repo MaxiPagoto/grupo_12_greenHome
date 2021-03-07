2c63bceaf4753b73bd7304274bfc5bbf08d3e2ef
alepizarro32 <alepizarro32@gmail.com>
2021-03-07 18:33:22 -0300

    Credenciales DB

--- database/config/config.js
+++ database/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAM,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_USERNAME,
